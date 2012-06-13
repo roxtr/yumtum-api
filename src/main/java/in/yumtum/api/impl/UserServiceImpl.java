6077fca033fac06b24e018ef030402a1ad4a8a34
hareesh-roxtr <hareesh@roxtr.com>
2012-06-13 08:49:57 +0530

    user-api impl

---
+++ src/main/java/in/yumtum/api/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package in.yumtum.api.impl;
+
+import in.yumtum.api.interfaces.UserService;
+import in.yumtum.api.vo.ResultVO;
+import in.yumtum.api.vo.UserVO;
+
+public class UserServiceImpl implements UserService {
+
+	public ResultVO createUser(UserVO user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResultVO validateUser(UserVO user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResultVO updateUser(UserVO user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResultVO forgotPassword(UserVO user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/main/java/in/yumtum/api/vo/UserVO.java
+++ src/main/java/in/yumtum/api/vo/UserVO.java
@@ -1,5 +1,84 @@
 package in.yumtum.api.vo;
 
 public class UserVO {
-
+	
+	private String address;
+	private String city;
+	private String email;
+	private String fName;
+	private String lName;
+	private String locality;
+	private String name;
+	private String password;
+	private String phone;
+	private String restaurantsOwned;
+	private String userId;
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getLocality() {
+		return locality;
+	}
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getRestaurantsOwned() {
+		return restaurantsOwned;
+	}
+	public void setRestaurantsOwned(String restaurantsOwned) {
+		this.restaurantsOwned = restaurantsOwned;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
 }
