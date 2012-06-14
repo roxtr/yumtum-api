868a92c3e6e7b96113e0dcfffdf78c2480da40a6
hareesh-roxtr <hareesh@roxtr.com>
2012-06-14 09:09:11 +0530

    updates for user validation

--- src/main/java/in/yumtum/api/service/impl/UserServiceImpl.java
+++ src/main/java/in/yumtum/api/service/impl/UserServiceImpl.java
@@ -1,5 +1,16 @@
 package in.yumtum.api.service.impl;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.access.DataContext;
+import org.apache.cayenne.exp.Expression;
+import org.apache.cayenne.query.SelectQuery;
+
+import in.yumtum.api.cayenne.persistent.YtRestUser;
 import in.yumtum.api.service.UserService;
 import in.yumtum.api.vo.ResultVO;
 import in.yumtum.api.vo.UserVO;
@@ -10,14 +21,41 @@ public class UserServiceImpl implements UserService {
 		
 		ResultVO result = validateUser(user);
 		
-		return result;
+		
+		
+		return null;
 	}
 
 	public ResultVO validateUser(UserVO user) {
 		
+		ObjectContext context = DataContext.createDataContext();
 		
+		ResultVO result = new ResultVO();
+
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("name", user.getName());
+		params.put("email", user.getEmail());
+		params.put("phone", user.getPhone());
+
+		Expression qualifier = Expression.fromString("name = $name or email = $email or phone = $phone");
 		
-		return null;
+		qualifier = qualifier.expWithParameters(params);
+		
+		SelectQuery select = new SelectQuery(YtRestUser.class, qualifier);
+		List userList = context.performQuery(select);
+		
+		if(userList.size() > 0){
+			
+			result.setIfError(true);
+			result.setError("User already exists");
+			
+		}else{
+			result.setIfError(false);
+		}
+		
+		System.out.println(result.isIfError());
+		
+		return result;
 	}
 
 	public ResultVO updateUser(UserVO user) {
@@ -30,4 +68,34 @@ public class UserServiceImpl implements UserService {
 		return null;
 	}
 
+
+	public boolean checkEmail(UserVO user) {
+		
+		return false;
+	}
+
+	public boolean checkPhone(UserVO user) {
+		
+		return false;
+	}
+
+	public boolean checkUName(UserVO user) {
+		
+		return false;
+	}
+	
+	public static void main(String args[]){
+		
+		UserVO user = new UserVO();
+		
+		user.setName("hareesh");
+		user.setEmail("makamhareesh@gmail.com");
+		//user.setPhone("9052228181");
+		
+		UserServiceImpl userSImpl = new UserServiceImpl();
+		
+		userSImpl.validateUser(user);
+		
+		
+	}
 }
--- src/main/java/in/yumtum/api/vo/ResultVO.java
+++ src/main/java/in/yumtum/api/vo/ResultVO.java
@@ -2,4 +2,22 @@ package in.yumtum.api.vo;
 
 public class ResultVO {
 
+	private String Error;
+	private boolean IfError;
+	
+	public String getError() {
+		return Error;
+	}
+	public void setError(String error) {
+		Error = error;
+	}
+	public boolean isIfError() {
+		return IfError;
+	}
+	public void setIfError(boolean ifError) {
+		IfError = ifError;
+	}
+	
+
+
 }
