450ca7cb9fd25e93db0deccde0dfe127bc5d54e1
hareesh-roxtr <hareesh@roxtr.com>
2012-06-19 09:31:24 +0530

    new file for restaurant service impl

---
+++ src/main/java/in/yumtum/api/service/impl/RestaurantServiceImpl.java
@@ -0,0 +1,29 @@
+package in.yumtum.api.service.impl;
+
+import in.yumtum.api.service.RestaurantService;
+import in.yumtum.api.vo.RestaurantVO;
+import in.yumtum.api.vo.ResultVO;
+
+public class RestaurantServiceImpl implements RestaurantService {
+
+	public ResultVO createRestaurant() {
+		
+		return null;
+	}
+
+	public ResultVO updateRestaurant() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResultVO deactivateRestaurant() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public RestaurantVO getRestaurantDetails(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
